b4b01fd5009506bc57713c1fafaf8d9d1da36f81
Zack Frank <frank.zack@gmail.com>
2018-10-13 00:59:52 -0500

    Light refactoring: remove instance variable element from first_half and second_half methods and rename them as such, then use them in constructing strings for verse and verses methods; dry up song method to call verses(99, 0)

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,11 +1,9 @@
 class Bottles
   def verse(num)
     define_vessels(num)
-    define_first_half_of_verse(num)
-    define_second_half_of_verse(num)
     <<-VERSE
-#{@first_half}
-#{@second_half}
+#{first_half(num)}
+#{second_half(num)}
 VERSE
   end
 
@@ -19,21 +17,21 @@ VERSE
     end
   end
 
-  def define_first_half_of_verse(num)
+  def first_half(num)
     if num >= 1
-      @first_half = "#{num} #{@vessel1} of beer on the wall, #{num} #{@vessel1} of beer."
+      "#{num} #{@vessel1} of beer on the wall, #{num} #{@vessel1} of beer."
     elsif num == 0
-      @first_half = "No more #{@vessel1} of beer on the wall, no more #{@vessel1} of beer."
+      "No more #{@vessel1} of beer on the wall, no more #{@vessel1} of beer."
     end
   end
 
-  def define_second_half_of_verse(num)
+  def second_half(num)
     if num > 1
-      @second_half = "Take one down and pass it around, #{num-1} #{@vessel2} of beer on the wall."
+      "Take one down and pass it around, #{num-1} #{@vessel2} of beer on the wall."
     elsif num == 1
-      @second_half = "Take it down and pass it around, no more #{@vessel2} of beer on the wall."
+      "Take it down and pass it around, no more #{@vessel2} of beer on the wall."
     elsif num == 0
-      @second_half = "Go to the store and buy some more, 99 #{@vessel2} of beer on the wall."
+      "Go to the store and buy some more, 99 #{@vessel2} of beer on the wall."
     end
   end
 
@@ -41,11 +39,9 @@ VERSE
     verses_string = ""
     until num1 < num2
       define_vessels(num1)
-      define_first_half_of_verse(num1)
-      define_second_half_of_verse(num1)
-      verses_string << @first_half
+      verses_string << first_half(num1)
       verses_string << "\n"
-      verses_string << @second_half
+      verses_string << second_half(num1)
       if (num1 - 1) >= num2 
         verses_string << "\n\n"
       end
@@ -57,22 +53,6 @@ VERSES
   end
 
   def song
-    num = 99
-    verses_string = ""
-    until num < 0
-      define_vessels(num)
-      define_first_half_of_verse(num)
-      define_second_half_of_verse(num)
-      verses_string << @first_half
-      verses_string << "\n"
-      verses_string << @second_half
-      if (num - 1) >= 0 
-        verses_string << "\n\n"
-      end
-      num -= 1
-    end
-    <<-SONG
-#{verses_string}
-SONG
+    verses(99, 0)
   end
 end
